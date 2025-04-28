66cde6263f964faee488a4881ef22cf69aa24e0e
Tristan <tristapayong@gmail.com>
2025-04-28 09:43:09 +0800

    Preparing for deployment

--- app/(main)/transaction/_components/receipt-scanner.jsx
+++ app/(main)/transaction/_components/receipt-scanner.jsx
@@ -22,8 +22,8 @@ const ReceiptScanner = ({onScanComplete}) => {
     } = useFetch(scanReceipt);
 
     const handleReceiptScan = async(file) => {
-        if(file.size > 5 * 1024 *1024) {
-            toast.error("File size should be less than 5MB.");
+        if(file.size > 15 * 1024 *1024) {
+            toast.error("File size should be less than 15MB.");
             return;
         }
 
